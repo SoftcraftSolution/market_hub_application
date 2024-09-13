0895f688d4eb4ee6faea9d3789eb20bd9391227c
SoftcraftSolution <168642737+SoftcraftSolution@users.noreply.github.com>
2024-09-13 17:23:32 +0530

    splash screen ui

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:market_hub_application/utility/theme.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: AppTheme.themeData["backgroundColor"],
+      body: Center(child: Image.asset("assets/splash_screeen_img/logo.png",height: 270,),),
+    );
   }
 }
---
+++ lib/utility/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static Map<String, dynamic> themeData={
+    "primeryColor":Color(0xffE76821),
+    "backgroundColor":Color(0xffffffff)
+  };
+}
