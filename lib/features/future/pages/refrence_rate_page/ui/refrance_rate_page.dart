74ff3741f33e806c43da85d5c4a33c14f0a3546e
SoftcraftSolution <168642737+SoftcraftSolution@users.noreply.github.com>
2024-10-19 10:02:17 +0530

    test

---
+++ lib/features/future/pages/refrence_rate_page/ui/refrance_rate_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RefranceRatePage extends StatelessWidget {
+  const RefranceRatePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
